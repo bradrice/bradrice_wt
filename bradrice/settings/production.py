1f8bf939579be5ac87569db45bc1c9740d6aea57
Brad Rice <bradrice1@gmail.com>
2024-07-23 17:09:58 -0400

    added a new host in Allowed

--- bradrice/settings/production.py
+++ bradrice/settings/production.py
@@ -10,16 +10,16 @@ env = environ.Env(
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
 
-print(env)
+print(dir(env))
 
 DJANGO_ROOT = '/home/app/'
 
 SECRET_KEY = env("SECRET_KEY")
-DEBUG = env("DEBUG")
+DEBUG = False
 ALLOWED_HOSTS = ["icons.bradrice.com", "localhost", "http://0.0.0.0:8009"]
 # ALLOWED_HOSTS = ["localhost", "localhost:8083", "127.0.0.1", "icons.bradrice.com", "[::1]"]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8083', 'http://localhost:8000', 'https://icons.bradrice.com', 'http://icons.bradrice.com']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8083', 'http://localhost:8000', 'http://0.0.0.0:8009', 'https://icons.bradrice.com', 'http://icons.bradrice.com']
 
 
 
